95ad9e558d1ea1dedc89765c4d6fbdb9c542c490
urmila4462 <dasari.urmila4462@gmail.com>
2022-06-01 15:15:22 +0530

    Added inserted the 30 element in between 56 and 70

--- LinkedList/src/com/bridgelabzLinkedList/LinkListMain.java
+++ LinkedList/src/com/bridgelabzLinkedList/LinkListMain.java
@@ -5,8 +5,8 @@ public class LinkListMain {
         System.out.println("Welcome To Linked List Program");
         LinkedList list=new LinkedList();
         list.add(56);
-        list.add(30);
         list.add(70);
+        list.insert(list.head, 30);
         list.display();
 
     }
--- LinkedList/src/com/bridgelabzLinkedList/LinkedList.java
+++ LinkedList/src/com/bridgelabzLinkedList/LinkedList.java
@@ -1,36 +1,45 @@
 package com.bridgelabzLinkedList;
 
- class LinkedList {
-        Node node;
-        Node tail;
-        Node head;
+public class LinkedList {
+    Node node;
+    Node tail;
+    Node head;
 
-        public void add(int data) {
-            Node newnode = new Node(data);
-            if (head == null) {
-                this.node = newnode;
-                this.head = newnode;
-                this.tail = newnode;
-            } else {
-                tail.next= newnode;
-                tail = newnode;
-            }
+    public void add(int data) {
+        Node newnode = new Node(data);
+        if (head == null) {
+            this.node = newnode;
+            this.head = newnode;
+            this.tail = newnode;
+        } else {
+            tail.next = newnode;
+            tail=newnode;
         }
+    }
 
-        public void display() {
-            Node current = head;
-            if (head == null) {
-                System.out.println("List is Empty");
-                return;
-            } else {
-                System.out.println("Nodes of singly linked list:");
-                while (current != null) {
-                    System.out.print(current.data + " ");
-                    current = current.next;
-                }
-                System.out.println();
-            }
+    public void insert(Node previousNode, int data) {
+        if (previousNode == null) {
+            System.out.println("previous node can't be empty");
+            return;
         }
-
+        Node newNode = new Node(data);
+        newNode.next = previousNode.next;
+        previousNode.next = newNode;
 
     }
+
+    public void display() {
+        Node current = head;
+        if (head == null) {
+            System.out.println("List is Empty");
+            return;
+        } else {
+            System.out.println("Nodes of simple linked list:");
+            while (current != null) {
+                System.out.println(current.data + " ");
+                current = current.next;
+            }
+            System.out.println();
+        }
+    }
+}
