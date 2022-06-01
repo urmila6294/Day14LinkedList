d5aca13372f8e34b93c7fbc8f7fe40d6d90c0803
urmila4462 <dasari.urmila4462@gmail.com>
2022-06-01 15:56:29 +0530

    Added orderlist method and displayed in ascending order

--- LinkedList/src/com/bridgelabzLinkedList/LinkListMain.java
+++ LinkedList/src/com/bridgelabzLinkedList/LinkListMain.java
@@ -4,19 +4,39 @@ public class LinkListMain {
     public static void main(String[] args) {
         System.out.println("Welcome To Linked List Program");
         System.out.println("Nodes of simple linked list:");
-        LinkedList list = new LinkedList();
-        list.add(56);
-        list.add(70);
-        list.display();
-        System.out.println("30 is Inserting between 56 and 70");
-        list.insert(list.head, 30);
-        list.display();
-        //list.pop();
-        //list.display();
-        list.searchNode(30);
-        list.display();
-        System.out.println("40 is Inserting after 30 ");
-        list.insert(list.head.next, 40);
-        list.display();
+        //LinkedList list = new LinkedList();
+        // list.add(56);
+        // list.add(70);
+        // list.displayNode();
+        // System.out.println("30 is Inserting between 56 and 70");
+        // list.insert(list.head, 30);
+        // list.displayNode();
+        // list.pop();
+        // list.displayNode();
+        // list.searchNode(30);
+        // list.displayNode();
+        // System.out.println("40 is Inserting after 30 ");
+        // list.insert(list.head.next, 40);
+        // list.displayNode();
+        // list.searchNode(40);
+        // list.displayNode();
+        // System.out.println("40 is Deleted ");
+        // list.deleteNode(40);
+        // list.displayNode();
+        // list.size();
+        // list.sortNode();
+        // System.out.println("Node is sorted in Ascending order");
+        // list.displayNode();
+
+        OrderedList ascendingList = new OrderedList();
+        ascendingList.adds(56);
+        ascendingList.adds(30);
+        ascendingList.adds(40);
+        ascendingList.adds(70);
+        ascendingList.display();
+        ascendingList.sortNode();
+        ascendingList.display();
+
+
     }
 }
